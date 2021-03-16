8c749096d9d7857b021d778adf33feba4ca7ccb4
Ahmed Anas <ahmed.a.tamim@gmail.com>
2021-03-16 21:00:02 +0500

    added and imported necessary files

---
+++ src/gatling/scala/frontline/common/Helper.scala
@@ -0,0 +1,9 @@
+package frontline.common
+
+object Helper {
+  def getProperty(propertyName: String, defaultValue: String): String = {
+    Option(System.getenv(propertyName))
+      .orElse(Option(System.getProperty(propertyName)))
+      .getOrElse(defaultValue)
+  }
+}
--- src/gatling/scala/frontline/sample/BasicSimulation.scala
+++ src/gatling/scala/frontline/sample/BasicSimulation.scala
@@ -10,6 +10,17 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
 
+import com.trilogy.ccab.grpc.api.{ChargingEngineGrpc, ChargingRequest, ChargingRequestType, ChargingResponse}
+import com.github.phisgr.gatling.grpc.Predef._
+import com.github.phisgr.gatling.grpc.action.GrpcCallActionBuilder
+import com.github.phisgr.gatling.grpc.protocol.StaticGrpcProtocol
+import com.github.phisgr.gatling.javapb._
+import frontline.common.Helper
+import io.gatling.core.feeder.BatchableFeederBuilder
+import io.gatling.core.session.Expression
+import io.gatling.core.structure.ScenarioBuilder
+
+
 class BasicSimulation extends Simulation {
 
   val httpConf = http
